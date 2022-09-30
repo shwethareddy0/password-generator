888650c32fb2062e0a692e72130de5b71ce4a448
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-09-29 18:56:04 -0700

    update the code with IF statement to validate the criterias

--- script.js
+++ script.js
@@ -18,8 +18,10 @@ function generatePassword() {
   );
 
   //declare the variables for length, lowercase, uppercase, numeric and spl chars
-
+  //confirm whether or not lowercase charachters are included.
   var lowerCase = confirm("Do you want lowercase characters?");
+  //If the user doesnot want a loercase, remove the lowercase charachters set from the password.
+  //
   var upperCase = confirm("Do you want uppercase characters?");
   var numeric = confirm("Do you want numeric characters?");
   var specialCharacters = confirm("Do you want special characters?");
@@ -29,20 +31,32 @@ function generatePassword() {
   var upperCaseCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numericSet = "0123456789";
   var specialCharSet = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+  // if user confirms the selection of the lowercase, then include lowercase characters in the password else move to the next set of characters.
+
   var result = "";
+
   do {
-    var lowerCaseCharacter = lowerCaseCharSet.charAt(
-      Math.floor(Math.random() * lowerCaseCharSet.length)
-    );
-    var upperCaseCharacter = upperCaseCharSet.charAt(
-      Math.floor(Math.random() * upperCaseCharSet.length)
-    );
-    var numericCharacter = numericSet.charAt(
-      Math.floor(Math.random() * numericSet.length)
-    );
-    var specialCharacter = specialCharSet.charAt(
-      Math.floor(Math.random() * specialCharSet.length)
-    );
+    if (upperCase === true) {
+      upperCaseCharacter = upperCaseCharSet.charAt(
+        Math.floor(Math.random() * upperCaseCharSet.length)
+      );
+    }
+    if (lowerCase === true) {
+      lowerCaseCharacter = lowerCaseCharSet.charAt(
+        Math.floor(Math.random() * lowerCaseCharSet.length)
+      );
+    }
+    if (numeric === true) {
+      numericCharacter = numericSet.charAt(
+        Math.floor(Math.random() * numericSet.length)
+      );
+    }
+    if (specialCharacters === true) {
+      specialCharacter = specialCharSet.charAt(
+        Math.floor(Math.random() * specialCharSet.length)
+      );
+    }
     var midResult =
       lowerCaseCharacter +
       upperCaseCharacter +
@@ -50,6 +64,7 @@ function generatePassword() {
       specialCharacter;
     result = result + midResult;
   } while (result.length < passwordLength);
+  result = result.substring(0, passwordLength);
   return result;
   console.log(result);
 }
